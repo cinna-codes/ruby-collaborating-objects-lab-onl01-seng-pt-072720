b29e1c911d69ebbc61c4f444e3f1ca0d2da98e40
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-21 15:41:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ attr_accessor :name
     if self.all.find { |artist| artist.name == name }
       self.all.find { |artist| artist.name == name }
     else
-      name = Artist.new(name)
+      name = self.new(name)
     end
       return name
   end
