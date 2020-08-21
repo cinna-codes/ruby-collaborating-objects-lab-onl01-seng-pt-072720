8e014cd1d1ccb00f09916485a4e4adb2439bd6dd
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-21 15:15:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,28 @@
 class Artist
 attr_accessor :name
-  def initialize(name)
 
+  def initialize(name)
+    @name = name
   end
 
+  def self.all
+    
+  end
+
+  def add_song
+    
+  end
+
+  def songs
+    
+  end
+
+  def self.find_or_create_by_name
+    
+  end
+
+  def print_song
+    
+  end
 
 end
