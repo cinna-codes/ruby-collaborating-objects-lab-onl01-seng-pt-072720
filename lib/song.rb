47d641b59244a2ddae536dd5f380a2089639acae
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-21 16:19:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,8 @@ attr_accessor :name, :artist
   def self.new_by_filename(filename)
     new_song_array = []
     new_song_array << filename.split(" - ")
-    new_song = self.new
-    filename.artist.name = new_song_array[0]
-    filename.name = new_song_array[1]
+    new_song = self.new(new_song_array[1])
+    new_song.artist.name = new_song_array[0]
   end
 
 
