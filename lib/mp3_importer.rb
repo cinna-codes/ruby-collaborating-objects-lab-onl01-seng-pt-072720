0f0532e5f3bbc1d947d2f91bd3d667c0f9db585e
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-21 16:07:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ attr_accessor :path
   end
 
   def files
-    Dir.glob("#{@path}*.mp3").map { |item| item.gsub("#{path}", "") }
+    Dir.glob("#{@path}/*.mp3").map { |item| item.gsub("#{path}/", "") }
 
   end
 
